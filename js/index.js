b06aaf93a28a446b899bd5359277d54d7aba817b
颜奕 <yanyi@yanyideMacBook-Pro.local>
2017-11-07 16:28:58 +0800

    changing it's color..

--- js/index.js
+++ js/index.js
@@ -10,9 +10,104 @@ class Background {
     ctx.fillRect(0, 0, this.dimension.width, this.dimension.height)
   }
 
-  stepState() { return true }
+  stepState() { return false }
 }
 
+function abs(x) {
+  if(x > 0) return x;
+  return -x;
+}
+
+var getRandomColor = function(){
+  return  '#' +
+    (function(color){
+    return (color +=  '0123456789abcdef'[Math.floor(Math.random()*16)])
+      && (color.length == 6) ?  color : arguments.callee(color);
+  })('');
+}
+
+class UI {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.color = '#ff0000';
+    this.changecolor = 0;
+  }
+
+  draw() {
+    let a = 50;
+    this.ctx.beginPath();
+    this.ctx.moveTo(a,a-10);
+    this.ctx.bezierCurveTo(a+12,a-30,a+30,a-10,a+20,a+5);
+    this.ctx.lineTo(a,a+25);
+    this.ctx.lineTo(a-20,a+5);
+    this.ctx.bezierCurveTo(a-30,a-10,a-12,a-30,a,a-10);
+    this.ctx.closePath();
+    if(this.changecolor == 0) this.ctx.fillStyle = this.color;
+    else this.ctx.fillStyle = getRandomColor();
+    this.ctx.fill();
+  }
+
+}
+
+class Obj {
+  constructor(ctx, props) {
+    this.ctx = ctx;
+    this.center = props.center;
+    this.width = props.width;
+    this.change = 0;
+    this.cnt = 0;
+    this.intv = 0;
+    this.changecolor = 0;
+    this.color = getRandomColor();
+  }
+
+  draw() {
+    let ctx = this.ctx;
+    if(this.changecolor == 0) ctx.strokeStyle = this.color;
+    else ctx.strokeStyle = getRandomColor();
+    ctx.lineWidth = this.width / 2;
+    ctx.strokeRect(this.center.x - this.width / 4, this.center.y - this.width / 4, this.width / 2, this.width / 2)
+  }
+
+  checkCollision(player) {
+  //   console.log(player.center);
+    // console.log(this.center);
+     if (this.change == 1) {
+       ++this.cnt;
+       console.log(this.cnt);
+     }
+
+     if (this.cnt > this.intv) {
+        this.resetPos();
+        this.changecolor = (Math.random() < 0.2);
+        this.color = getRandomColor();
+        this.cnt = 0; this.change = 0;
+      }
+
+     if (this.change == 0 && abs(player.center.x - this.center.x) < 30 && abs(player.center.y - this.center.y) < 30 ) {
+       this.change = 1;
+       this.center = {x: -20, y: -20};
+       this.intv = Math.random() * 50 + 10;
+
+       player.changecolor = this.changecolor;
+       player.color = this.color;
+
+       console.log(player.center);
+       console.log(this.center);
+     }
+  }
+
+  resetPos() {
+    this.center = { x: Math.random() * window.innerWidth * 0.8 + 20,
+                    y: Math.random() * window.innerHeight * 0.8 + 20
+                  };
+  }
+
+  setpState() {
+    return true;qw
+  }
+
+}
 
 class Border {
   constructor(ctx, props) {
@@ -29,7 +124,7 @@ class Border {
     ctx.strokeRect(this.topLeft.x, this.topLeft.y, this.dimension.width, this.dimension.height)
   }
 
-  stepState() { return true }
+  stepState() { return this.cnt == 0; }
 }
 
 
@@ -40,11 +135,14 @@ class Player {
     this.width = props.width
     this.speed = props.speed
     this.commands = {}
+    this.changecolor = 0;
+    this.color = '#ef8354';
   }
 
   draw() {
     let ctx = this.ctx
-    ctx.strokeStyle = '#ef8354'
+    if(this.changecolor == 0) ctx.strokeStyle = this.color;
+    else ctx.strokeStyle = getRandomColor();
     ctx.lineWidth = this.width / 2
     ctx.strokeRect(this.center.x - this.width / 4, this.center.y - this.width / 4, this.width / 2, this.width / 2)
   }
@@ -67,8 +165,10 @@ class Player {
       this.center.x -= this.speed
       updated = true
     }
-    console.log(keys)
-    console.log(updated)
+    if(updated) {
+      console.log(keys)
+      console.log(updated)
+    }
     return updated
   }
 }
@@ -96,6 +196,16 @@ class Game {
       width: lineWidth
     })
 
+    this.obj = new Obj(ctx, {
+      center: {
+        x: dimension.width / 2,
+        y: dimension.height / 2
+      },
+      width: 30
+    })
+
+    this.ui = new UI(ctx)
+
     this.player = new Player(ctx, {
       center: {
         x: dimension.width / 2,
@@ -104,10 +214,10 @@ class Game {
       width: 20,
       speed: 10
     })
-    
+
     this.boundFn = {}
     this.boundFn.tick = this.tick.bind(this)
-    
+
     this.states = {
       downKeys: new Set(),
       needRedraw: true
@@ -118,6 +228,8 @@ class Game {
     this.background.draw()
     this.border.draw()
     this.player.draw()
+    this.obj.draw()
+    this.ui.draw()
   }
 
   keyDown(key) {
@@ -151,22 +263,26 @@ class Game {
   }
 
   stepState() {
-    if (this.states.downKeys.size === 0) return false
+//    if (this.states.downKeys.size === 0) return false
 
-    let updated = this.player.stepState(this.states.downKeys)
+    let updated = this.player.stepState(this.states.downKeys) || this.obj.setpState();
     // dummy operations just to give the idea of `|=`
     // updated |= this.border.stepState()
     // updated |= this.background.stepState()
+
     if (updated) {
       this.checkCollision()
+      this.obj.checkCollision(this.player)
     }
     return updated
   }
 
   tick() {
     if (this.states.needRedraw) {
+  //    console.log(this.player.center);
       this.draw()
     }
+
     this.states.needRedraw = this.stepState()
     window.requestAnimationFrame(this.boundFn.tick)
   }
@@ -190,6 +306,7 @@ function init() {
   let $canvas = document.getElementsByTagName('canvas')[0]
   $canvas.width = window.innerWidth
   $canvas.height = window.innerHeight
+
   let game = new Game(
     $canvas.getContext('2d'),
     {
